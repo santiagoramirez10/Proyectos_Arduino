d4a029826e95ec7bef817e40ecce6b0e429ca6e2
santiagoramirez10 <santiagoramirezperez96@gmail.com>
2025-12-30 20:25:05 -0500

    Sensor de humedad y temperatura DHT11

--- DHT11/DHT11.ino
+++ DHT11/DHT11.ino
@@ -1,19 +1,46 @@
 #include <dht.h>
-#define dht_apin A0
+ 
+
+ 
+#define dht_apin A0 // Analog Pin sensor is connected to
+ 
 dht DHT;
+ 
+
 void setup(){
-Serial.begin(9600);
-delay(500);
-Serial.println("Sensor de temperatura y humedad DHT11");
-delay(1000);
-} 
-void loop(){ 
-DHT.read11(dht_apin);
-Serial.print("Porcentaje de humedad = ");
-Serial.print(DHT.humidity);
-Serial.print("%  ");
-Serial.print("Temperatura = ");
-Serial.print(DHT.temperature);
-Serial.println("C  ");
-delay(5000);
-} 
+ 
+  Serial.begin(9600);
+ 
+  delay(500);//Delay to let system boot
+ 
+ 
+  delay(1000);//Wait before accessing Sensor
+ 
+}//end "setup()"
+ 
+
+ 
+void loop(){
+ 
+  //Start of Program 
+ 
+  DHT.read11(dht_apin);
+ 
+
+ 
+    Serial.print("Humedad=");
+ 
+    Serial.print(DHT.humidity);
+ 
+    Serial.print("%  / ");
+ 
+    Serial.print("Temperatura=");
+ 
+    Serial.print(DHT.temperature); 
+ 
+    Serial.println("°C");
+ 
+  delay(2000);//Wait 5 seconds before accessing sensor again.
+
+}
+
